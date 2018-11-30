582d006ce2b522bb139dfd96597d47b93f698a81
Ricardo <449273863@qq.com>
2018-11-30 20:51:49 +0800

    初始化项目

--- Travle/src/main.js
+++ Travle/src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  // 与components: { App: APP }一致,下面是es6的写法
   components: { App },
   template: '<App/>'
 })
