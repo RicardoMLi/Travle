51acbfca01a2f0944d181cb82eed9df2b128164f
Ricardo <449273863@qq.com>
2018-12-01 11:16:51 +0800

    添加头部组件

--- Travle/src/main.js
+++ Travle/src/main.js
@@ -4,16 +4,22 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
 
-new Vue({
+var vm = new Vue({
   el: '#app',
   router,
   // 与components: { App: APP }一致,下面是es6的写法
   components: { App },
   template: '<App/>'
+})
+
+Vue.use({
+	vm
 })
--- Travle/src/pages/home/Home.vue
+++ Travle/src/pages/home/Home.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>Hello world</div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default{
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
