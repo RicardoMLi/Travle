4cf6e7dc62b8728a57b385cb9a8065fdfdad7cf3
Ricardo <449273863@qq.com>
2018-12-06 15:51:07 +0800

    添加城市列表

--- Travle/src/pages/city/City.vue
+++ Travle/src/pages/city/City.vue
@@ -2,17 +2,23 @@
     <div>
         <city-header></city-header>
         <search-header></search-header>
+        <city-list></city-list>
+        <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import SearchHeader from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 export default{
   name: 'City',
   components: {
     CityHeader,
     SearchHeader,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
